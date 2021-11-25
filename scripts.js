f4f7200d758e94b9172cc0c8a6e377db6892bd88
Eoin Harkin <91472105+HarkyDev@users.noreply.github.com>
2021-11-25 21:44:34 +0000

    updated files

--- scripts.js
+++ scripts.js
@@ -4,11 +4,47 @@ const ApiKey = "e2201392"
 const ApiLink = "http://www.omdbapi.com/?i=tt3896198&apikey=e2201392"
 
 var requestUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=e2201392';
-
 var responseText = document.getElementById('response-text');
 
 
-var omdbArray = ['http://www.omdbapi.com/?s=Batman&page=1&apikey=e2201392','http://www.omdbapi.com/?s=Batman&page=2&apikey=e2201392']
+var searchTerm = "batman"
+
+// array values
+var search1 = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=e2201392`
+var search2 = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=e2201392`
+var search3 = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=e2201392`
+var search4 = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=e2201392`
+var search5 = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=e2201392`
+var search6 = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=e2201392`
+
+
+
+function addElement () {
+  // create a new div element
+  const newDiv = document.createElement("div");
+  console.log("code is running")
+
+  // and give it some content
+  const newContent = document.createTextNode("content");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div1");
+  document.querySelector(".wrapperRow1").appendChild(newDiv, currentDiv);
+//   currentDiv.classList(".hoveringCards +  i ");
+}
+
+
+
+
+
+
+
+
+// var omdbArray = [batman1,batman2,batman3,batman4,batman5,batman6]
+var omdbArray = [search1,search1]
 console.log("-------------------------------------------------------------------------------------------------")
 var fetchRequest = function(){
     for (var i = 0; i < omdbArray.length; i++){
@@ -19,14 +55,19 @@ var fetchRequest = function(){
                 return response.json();
             })
             .then(function (data) {
-                for (var x = 1; x < 10; x++){
+                   for (var x = 1; x < 10; x++){
                 console.log("MOVIE TITLE ------------" , data.Search[x].Title  );
                 console.log("MOVIE POSTER ------------" , data.Search[x].Poster);
                 const newImg = document.createElement("img");
                 const newContent = data.Search[x].Poster;
                 newImg.src = newContent;
                 const currentImg = document.getElementById("row1");
-                document.body.insertBefore(newImg, currentImg);
+                document.querySelector(".wrapperRow1").appendChild(newImg, currentImg);
+                ///// new shit
+                addElement()
+                        
+
+
                 //  Document.createElement("img",data.Search[x].Poster);
                 // document.getElementById(x).src = data.Search[x].Poster
             }
@@ -43,12 +84,13 @@ fetchRequest()
 
  // create a new div element
 
- // and give it some content
- const newContent = document.src(data.Search[x].Poster);
+//slide buttons
+const buttonRight = document.getElementById('slideRight');
+const buttonLeft = document.getElementById('slideLeft');
 
- // add the text node to the newly created div
- newDiv.appendChild(newContent);
-
- // add the newly created element and its content into the DOM
- const currentDiv = document.getElementById("div1");
- document.body.insertBefore(newDiv, currentDiv);
+buttonRight.onclick = function () {
+  document.getElementById('wrapperRow1').scrollLeft += 1800; //move content right
+};
+buttonLeft.onclick = function () {
+  document.getElementById('wrapperRow1').scrollLeft -= 1800; //move content left
+};
